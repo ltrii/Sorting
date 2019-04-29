3461581ed5a57a9a2f8f37413f100701f8d02e78
ltrii <lelandtr@gmail.com>
2019-04-29 13:39:12 -0700

    Bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,8 +21,9 @@ print(selection_sort([8,7,10,19,2,22,24,3]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    swaper = False
-    while swaper == False:
+    swaper = True
+    while swaper:
+        swaper = False
         for i in range(0, len(arr) - 1):
             bubble1 = arr[i]
             bubble2 = arr[i+1]
